99c95cf481950711e3ccd0d65481b33cd960763f
Ryan Johnson <rhino.citguy@gmail.com>
2017-11-12 15:54:01 -0600

    feat(progress): add ds-progress element

---
+++ src/components/progress/demo-progress.js
@@ -0,0 +1,9 @@
+if (document.getElementById('vue-progressDemo')) {
+    new Vue({
+        el: '#vue-progressDemo',
+        data: {
+            pctComplete: 42,
+            indeterminate: false
+        }
+    });
+}
