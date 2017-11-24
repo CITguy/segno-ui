43df17779a3909ce04c67a64ee997bf2cf723c0a
Ryan Johnson <rhino.citguy@gmail.com>
2017-11-23 23:26:08 -0600

    Updated: 2017-11-23 23:26:07

--- docs.js
+++ docs.js
@@ -60,24 +60,24 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 85);
+/******/ 	return __webpack_require__(__webpack_require__.s = 84);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 85:
+/***/ 84:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
+
+__webpack_require__(85);
 
 __webpack_require__(86);
 
 __webpack_require__(87);
 
 __webpack_require__(88);
-
-__webpack_require__(89);
 
 (function () {
     var hashAnchors = document.querySelectorAll('[href^="#"]');
@@ -92,7 +92,7 @@ __webpack_require__(89);
 
 /***/ }),
 
-/***/ 86:
+/***/ 85:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -109,7 +109,7 @@ if (document.getElementById('vue-busyDemo')) {
 
 /***/ }),
 
-/***/ 87:
+/***/ 86:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -127,7 +127,7 @@ if (document.getElementById('vue-progressDemo')) {
 
 /***/ }),
 
-/***/ 88:
+/***/ 87:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -144,7 +144,7 @@ if (document.getElementById('vue-tabpanelDemo')) {
 
 /***/ }),
 
-/***/ 89:
+/***/ 88:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
