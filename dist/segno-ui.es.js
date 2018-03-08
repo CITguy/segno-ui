ae8156b8273592ad495522a390fbbcfad4d7279e
Ryan Johnson <rhino.citguy@gmail.com>
2018-03-08 01:07:00 -0600

    fix(polyfill): fix race condition

--- dist/segno-ui.es.js
+++ dist/segno-ui.es.js
@@ -876,9 +876,26 @@ var elements = Object.freeze({
 	DSTabsetElement: DSTabsetElement
 });
 
-function initialize () {
+function _defineElements () {
     for (let attr in elements) {
         elements[attr].$define();
+    }
+}
+
+function initialize () {
+    if (window.WebComponents) { // polyfill detected
+        if (window.WebComponents.ready) {
+            // polyfill already finished loading, initialize immediately
+            _defineElements();
+        } else {
+            // initialize when polyfill has finished loading
+            window.addEventListener('WebComponentsReady', function () {
+                _defineElements();
+            });
+        }
+    } else { // no polyfill detected
+        // initialize immediately
+        _defineElements();
     }
 }
 
--- src/segno/index.js
+++ src/segno/index.js
@@ -2,9 +2,26 @@ import { DSElement } from './elements/DSElement';
 
 import * as elements from './elements';
 
-function initialize () {
+function _defineElements () {
     for (let attr in elements) {
         elements[attr].$define();
+    }
+}
+
+function initialize () {
+    if (window.WebComponents) { // polyfill detected
+        if (window.WebComponents.ready) {
+            // polyfill already finished loading, initialize immediately
+            _defineElements();
+        } else {
+            // initialize when polyfill has finished loading
+            window.addEventListener('WebComponentsReady', function () {
+                _defineElements();
+            });
+        }
+    } else { // no polyfill detected
+        // initialize immediately
+        _defineElements();
     }
 }
 
